822423ba34e06526ed80ac2c9ba1f8da53580ae3
RMIT-VASH <S4181656@student.rmit.edu.au>
2025-02-20 14:10:42 +1100

    Week2

---
+++ Question2.java
@@ -0,0 +1,14 @@
+public class Question2 {
+    public static void main(String[] args) {
+        String day = "Wednesday";
+        String Month = "02";
+        int date = 19;
+        int year = 2025;
+
+        System.out.println(day + " " + Month + " " + date + " " + year);
+        System.out.println(day + " " + date + " " + Month + " " + year);
+        System.out.println(year + " " + Month + " " + date + " " + day);
+
+
+    }
+}
